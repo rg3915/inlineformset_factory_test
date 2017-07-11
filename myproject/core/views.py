60dd1e2afeba72ad60448cd148ad9829748144d4
rg3915 <rg3915@yahoo.com.br>
2017-07-11 16:13:42 -0300

    TaskAnswerCreate with CBV

--- myproject/core/views.py
+++ myproject/core/views.py
@@ -12,20 +12,28 @@ def home(request):
 
 def taskanswer(request):
     order_form = TaskAnswer()
-    item_order_formset = inlineformset_factory(
-        TaskAnswer, TaskAnswerFile, form=TaskAnswerFileForm, extra=0, can_delete=False,
-        min_num=1, validate_min=True)
-
+    item_order_formset = inlineformset_factory(TaskAnswer,
+                                               TaskAnswerFile,
+                                               form=TaskAnswerFileForm,
+                                               extra=0,
+                                               can_delete=False,
+                                               min_num=1,
+                                               validate_min=True
+                                               )
     if request.method == 'POST':
-        form = TaskAnswerForm(request.POST, request.FILES,
-                              instance=order_form, prefix='main')
-        formset = item_order_formset(
-            request.POST, request.FILES, instance=order_form, prefix='taskanswer')
-
+        form = TaskAnswerForm(request.POST,
+                              request.FILES,
+                              instance=order_form,
+                              prefix='main'
+                              )
+        formset = item_order_formset(request.POST,
+                                     request.FILES,
+                                     instance=order_form,
+                                     prefix='taskanswer'
+                                     )
         if form.is_valid() and formset.is_valid():
             form = form.save()
             formset.save()
-            print(formset)
             return HttpResponseRedirect(resolve_url('taskanswer1'))
     else:
         form = TaskAnswerForm(instance=order_form, prefix='main')
@@ -40,29 +48,45 @@ def taskanswer(request):
 
 
 class TaskAnswerCreate(TemplateView):
-    template_name = 'index2.html'
+    template_name = 'taskanswer2.html'
 
     def get_context_data(self, **kwargs):
         context = super(TaskAnswerCreate, self).get_context_data(**kwargs)
-        obj = self.object
-
-        item_order_formset = inlineformset_factory(
-            TaskAnswer, TaskAnswerFile, form=TaskAnswerFileForm, extra=0,
-            can_delete=False, min_num=1, validate_min=True)
-        formset = item_order_formset(
-            instance=TaskAnswer(), prefix='taskanswer')
+        order_form = TaskAnswer()
+        item_order_formset = inlineformset_factory(TaskAnswer,
+                                                   TaskAnswerFile,
+                                                   form=TaskAnswerFileForm,
+                                                   extra=0,
+                                                   can_delete=False,
+                                                   min_num=1,
+                                                   validate_min=True
+                                                   )
+        form = TaskAnswerForm(instance=order_form, prefix='main')
+        formset = item_order_formset(instance=order_form,
+                                     prefix='taskanswer'
+                                     )
+        context['order_form'] = order_form
+        context['item_order_formset'] = item_order_formset
+        context['form'] = form
         context['formset'] = formset
         return context
 
     def post(self, request, *args, **kwargs):
-
-        obj = self.get_object()
-
-        # if 'apply_btn' in request.POST:
+        context = self.get_context_data()
+        order_form = context['order_form']
+        item_order_formset = context['item_order_formset']
         if self.request.POST:
-            item_order_formset = inlineformset_factory(
-                TaskAnswer, TaskAnswerFile, form=TaskAnswerFileForm, extra=0,
-                can_delete=False, min_num=1, validate_min=True)
-            formset = item_order_formset(
-                instance=TaskAnswer(), prefix='taskanswer')
-            formset.save()
+            form = TaskAnswerForm(self.request.POST,
+                                  self.request.FILES,
+                                  instance=order_form,
+                                  prefix='main'
+                                  )
+            formset = item_order_formset(self.request.POST,
+                                         self.request.FILES,
+                                         instance=order_form,
+                                         prefix='taskanswer'
+                                         )
+            if form.is_valid() and formset.is_valid():
+                form = form.save()
+                formset.save()
+                return HttpResponseRedirect(resolve_url('taskanswer2'))
