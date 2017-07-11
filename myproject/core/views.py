0f5d18bbb5a0247a1b9c896825b904dee84bd3e2
rg3915 <rg3915@yahoo.com.br>
2017-07-10 21:16:52 -0300

    Usando CBV

--- myproject/core/views.py
+++ myproject/core/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render, resolve_url
 from django.http import HttpResponseRedirect
 from django.forms.models import inlineformset_factory
+from django.views.generic import TemplateView
 from .models import TaskAnswer, TaskAnswerFile
 from .forms import TaskAnswerForm, TaskAnswerFileForm
 
 
-def home(request):
+def taskanswer(request):
     order_forms = TaskAnswer()
     item_order_formset = inlineformset_factory(
         TaskAnswer, TaskAnswerFile, form=TaskAnswerFileForm, extra=0, can_delete=False,
@@ -13,14 +14,14 @@ def home(request):
 
     if request.method == 'POST':
         forms = TaskAnswerForm(request.POST, request.FILES,
-                         instance=order_forms, prefix='main')
+                               instance=order_forms, prefix='main')
         formset = item_order_formset(
             request.POST, request.FILES, instance=order_forms, prefix='taskanswer')
 
         if forms.is_valid() and formset.is_valid():
             forms = forms.save()
             formset.save()
-            return HttpResponseRedirect(resolve_url('home'))
+            return HttpResponseRedirect(resolve_url('taskanswer1'))
     else:
         forms = TaskAnswerForm(instance=order_forms, prefix='main')
         formset = item_order_formset(instance=order_forms, prefix='taskanswer')
@@ -32,3 +33,29 @@ def home(request):
 
     return render(request, 'index.html', context)
 
+
+class TaskAnswerCreate(TemplateView):
+    template_name = 'index2.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(TaskAnswerCreate, self).get_context_data(**kwargs)
+        obj=self.object
+        
+        item_order_formset = inlineformset_factory(
+            TaskAnswer, TaskAnswerFile, form=TaskAnswerFileForm, extra=0,
+            can_delete=False, min_num=1, validate_min=True)
+        formset = item_order_formset(instance=TaskAnswer(), prefix='taskanswer')
+        context['formset'] = formset
+        return context
+
+    def post(self, request, *args, **kwargs):
+
+        obj = self.get_object()
+        
+        # if 'apply_btn' in request.POST:
+        if self.request.POST:
+            item_order_formset = inlineformset_factory(
+                TaskAnswer, TaskAnswerFile, form=TaskAnswerFileForm, extra=0,
+                can_delete=False, min_num=1, validate_min=True)
+            formset = item_order_formset(instance=TaskAnswer(), prefix='taskanswer')
+            formset.save()
--- myproject/urls.py
+++ myproject/urls.py
@@ -2,10 +2,11 @@ from django.conf.urls import include, url
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from myproject.core.views import home
+from myproject.core.views import taskanswer, TaskAnswerCreate
 
 
 urlpatterns = [
-    url(r'^$', home, name='home'),
+    url(r'^taskanswer1/$', taskanswer, name='taskanswer1'),
+    url(r'^taskanswer2/$', TaskAnswerCreate.as_view(), name='taskanswer2'),
     url(r'^admin/', include(admin.site.urls)),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
